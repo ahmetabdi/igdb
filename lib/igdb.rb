731af20a1a5e401c9bfc89b71d269abd18c0b433
Ahmet <ahmetabdi@gmail.com>
2015-07-29 19:44:04 +0100

    Update igdb.rb

--- lib/igdb.rb
+++ lib/igdb.rb
@@ -2,7 +2,7 @@ require 'require_all'
 require_all 'lib'
 
 module Igdb
-  def self.connect(api_key = 'egBf-guz6Z8ZIMYw0GMykId-V0-OumqBwy988YFyq2E')
+  def self.connect(api_key)
     Igdb::Configuration::Api.instance.tap do |api|
       api.connect(api_key)
     end

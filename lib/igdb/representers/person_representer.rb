cdb01371158386f6dfcee5723856992d26181a8c
Ahmet Abdi <ahmetabdi@gmail.com>
2015-07-31 08:48:48 +0100

    Add slug to Person

--- lib/igdb/representers/person_representer.rb
+++ lib/igdb/representers/person_representer.rb
@@ -6,5 +6,6 @@ module PersonRepresenter
   property :dob # The day of birth of the person ISO standard is followed.
   property :country # The country of the person in http://en.wikipedia.org/wiki/ISO_3166-1_numeric .
   property :bio # The bio of the person.
+  property :slug # Not listed in the API
   property :size # The number of people in the database.
 end
